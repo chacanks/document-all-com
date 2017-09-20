bfa7688ddb362cb67dde78893150322615cdf5dd
chacanks <chacanks@gmail.com>
2017-09-20 12:54:20 +0900

    layout add

---
+++ src/view/layout/MainLayout.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import AppBar from 'material-ui/AppBar';
+
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class MainLayout extends Component {
+
+    
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+
+  render() {
+    return (
+      <div>
+        <AppBar
+            title="Document-All-Com"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer 
+        docked={false}
+        width={200}
+        open={this.state.open}
+        onRequestChange={(open) => this.setState({open})}>
+          <MenuItem>요구사항 관리</MenuItem>
+          <MenuItem>메뉴</MenuItem>
+          <MenuItem>메뉴</MenuItem>
+          <MenuItem>메뉴</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+export default MainLayout;
